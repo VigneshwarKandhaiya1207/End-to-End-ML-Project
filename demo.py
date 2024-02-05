814ed16b5d93dbf2e212585ca56f79303b9aea52
Vigneshwar Kandhaiya <vigneshwar.k2@gmail.com>
2024-02-06 01:02:47 +0530

    Dataingestion Pipeline

--- demo.py
+++ demo.py
@@ -1,6 +1,14 @@
-from mlProject.config.mongo_db_connection import MongoDBClient
-from mlProject.logger import logging
-from mlProject.exceptions import CustomException
-import sys
+# from mlProject.config.mongo_db_connection import MongoDBClient
+# from mlProject.logger import logging
+# from mlProject.exceptions import CustomException
+# import sys
 
-instance = MongoDBClient()
+# instance = MongoDBClient()
+
+
+from mlProject.entity.artifact_entity import DataIngestionArtifact
+from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.components.data_ingestion import DataIngestion
+
+di_ins=DataIngestion(DataIngestionConfig)
+di_art=di_ins.initiate_data_ingestion()
