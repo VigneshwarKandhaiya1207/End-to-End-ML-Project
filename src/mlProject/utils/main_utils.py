bf723610cf1f5ae432988e11d2fb46e55eec945f
Vigneshwar Kandhaiya <vigneshwar.k2@gmail.com>
2024-02-08 08:01:03 +0530

    Data validation bug fixes

--- src/mlProject/utils/main_utils.py
+++ src/mlProject/utils/main_utils.py
@@ -22,9 +22,9 @@ def write_yaml_file(file_path: str, content: object, replace:bool=False)-> None:
         if replace:
             if os.path.exists(file_path):
                 os.remove(file_path)
-            os.makedirs(os.path.dirname(file_path),exist_ok=True)
-            with open(file_path,"w") as file:
-                yaml.dump(content,file)
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"w") as file:
+            yaml.dump(content,file)
     except Exception as e:
         raise CustomException(e,sys) from e
 
